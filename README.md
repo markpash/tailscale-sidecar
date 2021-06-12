d3922ac225a19ca9c7e96a6059952c626bd2e17e
Mark Pashmfouroush <mark@markpash.me>
2021-06-12 23:05:02 +0100

    poc -> usable

---
+++ README.md
@@ -0,0 +1,49 @@
+# tailscale-sidecar
+
+This is barely tested software, I don't guarantee it works but please make an issue if you use it and find a bug. Pull requests are welcome.
+
+This program is designed to expose services onto a tailscale network without needing root.
+Using the `tsnet` package provided by tailscale, we can listen on a port on a tailscale IP and then proxy the stream to a destination.
+The use-case for me was running this as a sidecar container in nomad to expose services onto my tailscale network, without needing root or routing.
+
+Currently this only supports tcp because right now because that's all I care about. I may try to make UDP work in the future.
+
+## Usage
+
+To use this program, it needs to be executed with a few environment variables. They are as follows:
+
+```bash
+TS_LOGIN
+TS_SIDECAR_STATEDIR
+TS_SIDECAR_NAME
+TS_SIDECAR_BINDINGS
+```
+
+On first run, you should run with `TS_LOGIN` set to `1` as this will output a login url in the logs, which you can use to authorise this instance and create the state. Once the instance is authorised, the variable no longer needs to be provided.
+
+`TS_SIDECAR_STATEDIR` is the location where the persistent data for the sidecar will be stored. This is used to not need to re-authorise the instance. In a container setup, you'll want to have this persisted. The default path is `./tsstate`.
+
+`TS_SIDECAR_NAME` is the name that you wish this program to use to present itself to the tailscale servers, this is what you will see in your panel.
+
+`TS_SIDECAR_BINDINGS` is the path to the bindings file, which should be a JSON file which has contents much like what's below.
+The default path for bindings is `/etc/ts-sidecar/bindings.json`.
+
+## Configuration
+
+Configuration should look like this:
+
+```json
+[
+    {
+        "from": 80,
+        "to": "127.0.0.1:8000"
+    }
+]
+```
+
+## Disclaimer
+
+THIS IS NOT OFFICIALLY ENDORSED BY TAILSCALE.
+
+I thought I should put that there just in case someone thought it may be a tailscale product.
+I'm also not responsible for any of the bad things that might happen as a result of using this software. It works for me but maybe not for you.
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -10,19 +13,59 @@ import (
 	"tailscale.com/tsnet"
 )
 
-func main() {
-	err := os.Setenv("TAILSCALE_USE_WIP_CODE", "true")
-	if err != nil {
-		panic(err)
+type Binding struct {
+	From uint16 `json:"from"`
+	To   string `json:"to"`
+}
+
+func loadBindings() ([]Binding, error) {
+	bindingsPath := os.Getenv("TS_SIDECAR_BINDINGS")
+	if bindingsPath == "" {
+		bindingsPath = "/etc/ts-sidecar/bindings.json"
 	}
 
-	s := tsnet.Server{
-		Dir:      "./data",
-		Hostname: "test-sidecar",
-	}
-	ln, err := s.Listen("tcp", ":80")
+	bindingsFile, err := os.Open(bindingsPath)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+	defer bindingsFile.Close()
+
+	d := json.NewDecoder(bindingsFile)
+
+	var bindings []Binding
+	if err := d.Decode(&bindings); err != nil {
+		return nil, err
+	}
+
+	if len(bindings) == 0 {
+		return nil, errors.New("bindings empty")
+	}
+
+	return bindings, nil
+}
+
+func newTsNetServer() tsnet.Server {
+	hostname := os.Getenv("TS_SIDECAR_NAME")
+	if hostname == "" {
+		panic("TS_SIDECAR_NAME env var not set")
+	}
+
+	stateDir := os.Getenv("TS_SIDECAR_STATEDIR")
+	if stateDir == "" {
+		stateDir = "./tsstate"
+	}
+
+	return tsnet.Server{
+		Dir:      stateDir,
+		Hostname: hostname,
+	}
+}
+
+func proxyBind(s tsnet.Server, b Binding) {
+	ln, err := s.Listen("tcp", fmt.Sprintf(":%d", b.From))
+	if err != nil {
+		log.Println(err)
+		return
 	}
 
 	for {
@@ -35,14 +78,16 @@ func main() {
 		go func(left net.Conn) {
 			defer left.Close()
 
-			right, err := net.Dial("tcp", "127.0.0.1:8000")
+			right, err := net.Dial("tcp", b.To)
 			if err != nil {
 				log.Println(err)
 				return
 			}
 			defer right.Close()
 
+			var wg sync.WaitGroup
 			proxyConn := func(a, b net.Conn) {
+				defer wg.Done()
 				_, err := io.Copy(a, b)
 				if err != nil {
 					log.Println(err)
@@ -50,20 +95,36 @@ func main() {
 				}
 			}
 
-			var wg sync.WaitGroup
 			wg.Add(2)
-
-			go func() {
-				defer wg.Done()
-				proxyConn(right, left)
-			}()
-
-			go func() {
-				defer wg.Done()
-				proxyConn(left, right)
-			}()
+			go proxyConn(right, left)
+			go proxyConn(left, right)
 
 			wg.Wait()
 		}(conn)
 	}
+}
+
+func main() {
+	// Apparently this envvar needs to be set for this to work!
+	err := os.Setenv("TAILSCALE_USE_WIP_CODE", "true")
+	if err != nil {
+		panic(err)
+	}
+
+	bindings, err := loadBindings()
+	if err != nil {
+		panic(err)
+	}
+
+	s := newTsNetServer()
+
+	var wg sync.WaitGroup
+	for _, binding := range bindings {
+		wg.Add(1)
+		go func(binding Binding) {
+			defer wg.Done()
+			proxyBind(s, binding)
+		}(binding)
+	}
+	wg.Wait()
 }
